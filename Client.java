5c84ee3ef3e608b52abcf1917e878596594614ac
Berkay Aydemir <berkayaydmr@hotmail.com>
2025-06-05 19:01:49 +0300

    Implement client-server communication in Client class

--- Client.java
+++ Client.java
@@ -7,5 +7,14 @@ public class Client {
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(lbSocket.getOutputStream()));
         out.write("CLIENT\n");
         out.flush();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(lbSocket.getInputStream()));
+        String response = in.readLine();
+        if (response != null && response.startsWith("SERVER_PORT")) {
+            int port = Integer.parseInt(response.split(" ")[1]);
+            Socket serverSocket = new Socket("localhost", port);
+            BufferedReader serverIn = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+            System.out.println("Server says: " + serverIn.readLine());
+        }
     }
 }
